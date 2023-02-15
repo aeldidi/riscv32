8e1840656ff1de6c59a13f3a070dc2a5cbc3bd1c
Ayman El Didi <ayman@eldidi.org>
2023-02-15 15:00:27 -0700

    added riscv32_dumpcpu

--- src/main.c
+++ src/main.c
@@ -79,8 +79,6 @@ void
 riscv32_execute(struct riscv32_Cpu* cpu, const size_t instructions_len,
 		const uint32_t* instructions)
 {
-	// FIXME: The ecall instruction is used to exit, regardless of what's
-	//        in a7.
 	while (true) {
 		if (cpu->pc >= instructions_len) {
 			panic("out of bounds pc: %zu", cpu->pc);
@@ -89,6 +87,8 @@ riscv32_execute(struct riscv32_Cpu* cpu, const size_t instructions_len,
 		uint32_t opcode      = decode(instruction);
 		switch (opcode) {
 		case ECALL:
+			// FIXME: The ecall instruction is used to exit,
+			//        regardless of what's in a7.
 			return;
 		case LUI: {
 			uint32_t reg = (instruction >> 7) &
@@ -102,6 +102,45 @@ riscv32_execute(struct riscv32_Cpu* cpu, const size_t instructions_len,
 			panic("unimplemented opcode: %" PRIu32 "", opcode);
 		}
 	}
+}
+
+void
+riscv32_dumpcpu(const struct riscv32_Cpu* cpu)
+{
+	printf("ra:  %" PRId32 "\n", cpu->registers[RISCV32_RA]);
+	printf("sp:  %" PRId32 "\n", cpu->registers[RISCV32_SP]);
+	printf("gp:  %" PRId32 "\n", cpu->registers[RISCV32_GP]);
+	printf("tp:  %" PRId32 "\n", cpu->registers[RISCV32_TP]);
+	printf("t0:  %" PRId32 "\n", cpu->registers[RISCV32_T0]);
+	printf("t1:  %" PRId32 "\n", cpu->registers[RISCV32_T1]);
+	printf("t2:  %" PRId32 "\n", cpu->registers[RISCV32_T2]);
+	printf("s0:  %" PRId32 "\n", cpu->registers[RISCV32_S0]);
+	printf("s1:  %" PRId32 "\n", cpu->registers[RISCV32_S1]);
+	printf("a0:  %" PRId32 "\n", cpu->registers[RISCV32_A0]);
+	printf("a1:  %" PRId32 "\n", cpu->registers[RISCV32_A1]);
+	printf("a2:  %" PRId32 "\n", cpu->registers[RISCV32_A2]);
+	printf("a3:  %" PRId32 "\n", cpu->registers[RISCV32_A3]);
+	printf("a4:  %" PRId32 "\n", cpu->registers[RISCV32_A4]);
+	printf("a5:  %" PRId32 "\n", cpu->registers[RISCV32_A5]);
+	printf("a6:  %" PRId32 "\n", cpu->registers[RISCV32_A6]);
+	printf("a7:  %" PRId32 "\n", cpu->registers[RISCV32_A7]);
+	printf("s0:  %" PRId32 "\n", cpu->registers[RISCV32_S0]);
+	printf("s1:  %" PRId32 "\n", cpu->registers[RISCV32_S1]);
+	printf("s2:  %" PRId32 "\n", cpu->registers[RISCV32_S2]);
+	printf("s3:  %" PRId32 "\n", cpu->registers[RISCV32_S3]);
+	printf("s4:  %" PRId32 "\n", cpu->registers[RISCV32_S4]);
+	printf("s5:  %" PRId32 "\n", cpu->registers[RISCV32_S5]);
+	printf("s6:  %" PRId32 "\n", cpu->registers[RISCV32_S6]);
+	printf("s7:  %" PRId32 "\n", cpu->registers[RISCV32_S7]);
+	printf("s8:  %" PRId32 "\n", cpu->registers[RISCV32_S8]);
+	printf("s9:  %" PRId32 "\n", cpu->registers[RISCV32_S9]);
+	printf("s10: %" PRId32 "\n", cpu->registers[RISCV32_S10]);
+	printf("s11: %" PRId32 "\n", cpu->registers[RISCV32_S11]);
+	printf("t3:  %" PRId32 "\n", cpu->registers[RISCV32_T3]);
+	printf("t4:  %" PRId32 "\n", cpu->registers[RISCV32_T4]);
+	printf("t5:  %" PRId32 "\n", cpu->registers[RISCV32_T5]);
+	printf("t6:  %" PRId32 "\n", cpu->registers[RISCV32_T6]);
+	printf("pc:  %" PRId32 "\n", cpu->pc);
 }
 
 int
@@ -130,7 +169,6 @@ main(int argc, char** argv)
 	riscv32_execute(&cpu, executable_len, executable);
 
 	printf("execution completed\n");
-	for (size_t i = 0; i < 32; i += 1) {
-		printf("register %.2zu: %" PRId32 "\n", i, cpu.registers[i]);
-	}
+	printf("registers:\n");
+	riscv32_dumpcpu(&cpu);
 }
